f3dfb36aba0aaf69e17dd7c650c7c53727afe4f5
Даша Белова <Dashabelova003@yandex.ru>
2026-07-14 23:07:45 +0300

    ошибки проверки

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -6,8 +6,12 @@ import java.util.Optional;
 
 public interface FilmStorage {
     Film create(Film film);
+
     Film update(Film film);
+
     Collection<Film> findAll();
+
     Optional<Film> findById(Integer id);
+
     boolean existsById(Integer id);
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -6,8 +6,12 @@ import java.util.Optional;
 
 public interface UserStorage {
     User create(User user);
+
     User update(User user);
+
     Collection<User> findAll();
+
     Optional<User> findById(Integer id);
+
     boolean existsById(Integer id);
 }
