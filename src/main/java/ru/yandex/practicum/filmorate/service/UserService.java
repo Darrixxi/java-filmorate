fa497b40a5345c2b2edd5c4970d3dcaf95de0994
Даша Белова <Dashabelova003@yandex.ru>
2026-07-15 22:05:35 +0300

    ошибки проверки

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -10,7 +10,6 @@ import ru.yandex.practicum.filmorate.storage.user.UserStorage;
 
 import java.time.LocalDate;
 import java.util.Collection;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
