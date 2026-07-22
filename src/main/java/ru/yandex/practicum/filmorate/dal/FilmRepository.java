0409e72cea562315faed9cfb4a72b002fda2398d
Даша Белова <Dashabelova003@yandex.ru>
2026-07-22 11:08:43 +0300

    ошибки проверки

--- src/main/java/ru/yandex/practicum/filmorate/dal/FilmRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/FilmRepository.java
@@ -3,8 +3,6 @@ package ru.yandex.practicum.filmorate.dal;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
